67719b67c6c0b0d61b6f99a5dc3f85f9f2164058
ihsan ihsan <akilsiztahta3132@gmail.com>
2024-11-28 22:52:37 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Yazdırma Komutları
+
+
+
+            #endregion
+
+            Console.Read();
+
+        }
+    }
+}
