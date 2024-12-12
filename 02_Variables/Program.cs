e4183f0c2851b619ee6fb8c4fbf79ada590b1e05
ihsankasim <akilsiztahta3132@gmail.com>
2024-12-12 18:26:22 +0300

    Add files via upload

---
+++ 02_Variables/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region double değişkenler
+
+            //Console.WriteLine("    ***** Fiyat Listesi *****     ");
+            //Console.WriteLine();
+
+            //double
+            //appleprice = 14.25,
+            //orangeprice = 20.75,
+            //strawberryprice = 45,
+            //potatoprice = 9.74,
+            //tomatoprice = 6.88;
+
+            //Console.WriteLine(" -----> Elma Kilogram Fiyatı: " + appleprice + " TL");
+            //Console.WriteLine(" -----> Portakal Kilogram Fiyatı: " + orangeprice + " TL");
+            //Console.WriteLine(" -----> Çilek Kilogram Fiyatı: " + strawberryprice + " TL");
+            //Console.WriteLine(" -----> Patates Kilogram Fiyatı: " + potatoprice + " TL");
+            //Console.WriteLine(" -----> Domates Kilogram Fiyatı: " + tomatoprice + " TL");
+            //Console.WriteLine();
+
+            //double
+            //applekg = 3,
+            //orangekg = 4,
+            //strawberrykg = 1.250,
+            //potatokg = 3.650,
+            //tomatokg = 2.150;
+
+            //double
+            //appletotalprice = appleprice * applekg,
+            //orangetotalprice = orangeprice * orangekg,
+            //strawberrytotalprice = strawberryprice * strawberrykg,
+            //potatototalprice = potatoprice * potatokg,
+            //tomatotalprice = tomatoprice * tomatokg;
+
+            //Console.WriteLine("Alınan ürün : Elma " + " - Kg fiyatı: " + appleprice + " TL" + " - Alınan Miktar: " + applekg + " Kg" + " - Ödenecek Tutar: " + appletotalprice + " TL");
+            //Console.WriteLine("Alınan ürün : Portakal " + " - Kg fiyatı: " + orangeprice + " TL" + " - Alınan Miktar: " + orangekg + " Kg" + " - Ödenecek Tutar: " + orangetotalprice + " TL");
+            //Console.WriteLine("Alınan ürün : Çilek " + " - Kg fiyatı: " + strawberryprice + " TL" + " - Alınan Miktar: " + strawberrykg + " Kg" + " - Ödenecek Tutar: " + strawberrytotalprice + " TL");
+            //Console.WriteLine("Alınan ürün : Patates " + " - Kg fiyatı: " + potatoprice + " TL" + " - Alınan Miktar: " + potatokg + " Kg" + " - Ödenecek Tutar: " + potatototalprice + " TL");
+            //Console.WriteLine("Alınan ürün : Domates " + " - Kg fiyatı: " + tomatoprice + " TL" + " - Alınan Miktar: " + tomatokg + " Kg" + " - Ödenecek Tutar: " + tomatotalprice + " TL");
+
+            #endregion
+
+            #region Char değişkenler
+
+            //char symbol = 'a';
+
+            //Console.WriteLine(symbol);
+
+            #endregion
+
+            Console.Read();
+
+        }
+    }
+}
